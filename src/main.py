1c12dadc3b360e7e5bf2bbbdee2b2ee24b190f0b
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-08-03 22:23:17 +0200

    feat: use Flask to handle message

--- src/main.py
+++ src/main.py
@@ -2,13 +2,21 @@ import os
 import asyncio
 import logging
 
-from dotenv import load_dotenv
-
 from matrix_client import MatrixClient
 
-load_dotenv()
+from flask import Flask, request, jsonify
 
+# Load environment variables in development
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
+
+
+# Logging
 logging.basicConfig(level=logging.INFO)
+
 
 # Env file variables
 BACKUP_PATH = os.getenv("BACKUP_PATH")
@@ -18,17 +26,19 @@ USER_ID = os.getenv("USER_ID")
 USER_PASS = os.getenv("USER_PASS")
 
 ROOM_ID = os.getenv("ROOM_ID")
-MESSAGE = os.getenv("MESSAGE")
 
-async def main():
-    client = MatrixClient(
-        homeserver=HOMESERVER, 
-        user_id=USER_ID, 
-        config_path=f"{BACKUP_PATH}/config.json",
-        store_path=f"{BACKUP_PATH}/store/",
-        do_logging=True
-    )
-    
+
+# Initialize Matrix client
+client = MatrixClient(
+    homeserver=HOMESERVER, 
+    user_id=USER_ID, 
+    config_path=f"{BACKUP_PATH}/config.json",
+    store_path=f"{BACKUP_PATH}/store/",
+    do_logging=True
+)
+
+
+async def main(message):
     # Use full_state=True here to pull any room invites that occurred or
     # messages sent in rooms _before_ this program connected to the
     # Matrix server
@@ -36,10 +46,27 @@ async def main():
 
     await client.login(USER_PASS)
 
-    await client.send_simple_message(ROOM_ID, MESSAGE)
+    await client.send_simple_message(ROOM_ID, message)
 
     await client.close()
 
+
+# Create app
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def index():
+    try:
+        # Get message
+        message = request.get_json()["message"]
+
+        # TODO: Use Celery instead
+        asyncio.run(main(message))
+
+        return jsonify({"status": "success"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": e})
+
+
 if (__name__ == "__main__"):
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    app.run(debug=True) 
