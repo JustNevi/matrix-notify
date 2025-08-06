b14419182efe89e3d0391b0430e084cc5a7c7d33
JustNevi <slavkad25@gmail.com>
2025-08-06 18:54:31 +0200

    feat: add room id to test reqest and use env file

--- test/test_request.py
+++ test/test_request.py
@@ -1,8 +1,18 @@
 import requests
 
-ip = "127.0.0.1"
-port = "8000"
+from dotenv import load_dotenv
+load_dotenv()
 
-data = {"message": "Test message"}
-response = requests.post(f"http://{ip}:{port}/", json=data)
+SERVER_IP = os.getenv("SERVER_IP")
+SERVER_PORT = os.getenv("SERVER_PORT")
+
+ROOM_ID = os.getenv("ROOM_ID")
+TEST_MESSAGE = os.getenv("TEST_MESSAGE")
+
+data = {
+    "room_id": ROOM_ID, 
+    "message": TEST_MESSAGE
+}
+
+response = requests.post(f"http://{SERVER_IP}:{SERVER_PORT}/", json=data)
 print(response.json())
