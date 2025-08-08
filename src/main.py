42294fd71c4366c676281ae6c5a0f7ff64e3e97b
JustNevi <122728617+JustNevi@users.noreply.github.com>
2025-08-08 21:47:32 +0200

    feat: create backup dir if not exists

--- src/main.py
+++ src/main.py
@@ -20,6 +20,8 @@ logging.basicConfig(level=logging.INFO)
 
 # Env file variables
 BACKUP_PATH = os.getenv("BACKUP_PATH")
+CONFIG_PATH = os.path.join(BACKUP_PATH, "config.json")
+STORE_PATH = os.path.join(BACKUP_PATH, "store")
 
 HOMESERVER = os.getenv("HOMESERVER")
 USER_ID = os.getenv("USER_ID")
@@ -30,11 +32,22 @@ USER_PASS = os.getenv("USER_PASS")
 client = MatrixClient(
     homeserver=HOMESERVER, 
     user_id=USER_ID, 
-    config_path=f"{BACKUP_PATH}/config.json",
-    store_path=f"{BACKUP_PATH}/store/",
+    config_path=CONFIG_PATH,
+    store_path=STORE_PATH,
     do_logging=True
 )
 
+def setup():
+    if not os.path.exists(BACKUP_PATH):
+        os.makedirs(BACKUP_PATH)
+
+    if not os.path.exists(STORE_PATH):
+        os.makedirs(STORE_PATH)
+
+    if not os.path.exists(CONFIG_PATH):
+        with open(CONFIG_PATH, 'w') as f:
+            # Empty JSON object is crucial for python be able to load using "json" 
+            f.write('{}')
 
 async def main(room_id, message):
     await client.login(USER_PASS)
@@ -55,6 +68,8 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def index():
     try:
+        setup()
+
         data = request.get_json()
 
         # Get message and room
